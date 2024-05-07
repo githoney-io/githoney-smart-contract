6f56f82416a4f554b8e46a70d53e3657028decf9
sofia-bobbiesi <sofiabobbiesi@gmail.com>
2024-05-07 18:13:01 -0300

    Update mergeBounty function

--- offchain/src/operations/merge.ts
+++ offchain/src/operations/merge.ts
@@ -1,12 +1,27 @@
 import { buildGithoneyMintingPolicy, buildGithoneyValidator } from "../scripts";
 import { MIN_ADA, NetConfig } from "../constants";
-import { Data, fromText, toUnit } from "lucid-cardano";
+import { Data, fromText, toUnit, OutRef } from "lucid-cardano";
 
-async function mergeBounty(maintainerAddr: string, netConfig: NetConfig) {
+async function mergeBounty(
+  ref_input: OutRef,
+  maintainerAddr: string,
+  netConfig: NetConfig
+) {
   console.debug("START mergeBounty");
   const lucid = netConfig.lucidAdmin!;
   const gitHoneyValidator = buildGithoneyValidator();
   const validatorAddress = lucid.utils.validatorToAddress(gitHoneyValidator);
+  const [contractUtxo] = await lucid.utxosByOutRef([ref_input]);
+  const bountyDatum = Data.from<BountyDatumT>(
+    contractUtxo.datum as string,
+    BountyDatum as unknown as BountyDatumT
+  );
+  const newBountyDatum = {
+    ...bountyDatum,
+    merged: true
+  };
+
+  const githoneyAdrr = await lucid.wallet.address();
   const utxo = (await lucid.wallet.getUtxos())[0];
   const outRef = { txHash: utxo.txHash, outputIndex: utxo.outputIndex };
   const mintingScript = buildGithoneyMintingPolicy(outRef);
@@ -15,10 +30,22 @@ async function mergeBounty(maintainerAddr: string, netConfig: NetConfig) {
 
   // Your code here
 
+  // Your return object here
+  const tx = await lucid
+    .newTx()
+    // .collectFrom(utxos)
+    // .collectFrom(contractUtxo, CapsuleValidatorRedeemer.Deposit())
+    // .attachSpendingValidator(gitHoneyValidator)
+    .payToContract(validatorAddress, { inline: newBountyDatum }, {})
+    .payToAddress(maintainerAddr, { lovelace: MIN_ADA })
+    .payToAddress(githoneyAdrr, {})
+    .complete();
+
+  lucid.selectWalletFromSeed(netConfig.SEED);
+  const txSigned = await tx.sign().complete();
+
   console.debug("END mergeBounty");
-  return {
-    // Your return object here
-  };
+  return txSigned.toString();
 }
 
 export default mergeBounty;
