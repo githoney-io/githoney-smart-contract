0a832d87ad0a8284e25056f8df3998fd808f3c6d
sofia-bobbiesi <sofiabobbiesi@gmail.com>
2024-05-07 17:22:18 -0300

    Refactor createBounty function to include fee, rewards, deadline, and bounty_id

--- offchain/src/operations/create.ts
+++ offchain/src/operations/create.ts
@@ -1,10 +1,14 @@
 import { buildGithoneyMintingPolicy, buildGithoneyValidator } from "../scripts";
-import { Data, fromText, toUnit } from "lucid-cardano";
-import { NetConfig, Roles } from "../utils";
+import { Data, fromText, toUnit, Assets } from "lucid-cardano";
+import { NetConfig, Roles, MIN_ADA } from "../utils";
+import { BountyDatum, BountyDatumT } from "../types";
 
 async function createBounty(
   maintainerAddr: string,
-  amount: bigint,
+  fee: bigint,
+  rewards: Assets,
+  deadline: bigint,
+  bounty_id: string,
   netConfig: NetConfig
 ) {
   console.debug("START createBounty");
@@ -25,20 +29,44 @@ async function createBounty(
   const { [githoneyUnit]: _, ...mantainerAssets } = mintAssets;
   const maintainerPkh =
     lucid.utils.getAddressDetails(maintainerAddr).paymentCredential!.hash;
-  const githoneyPkh = lucid.utils.getAddressDetails(
-    await lucid.wallet.address()
-  ).paymentCredential!.hash;
+  const githoneyAdrr = await lucid.wallet.address();
+  const githoneyPkh =
+    lucid.utils.getAddressDetails(githoneyAdrr).paymentCredential!.hash;
 
   console.debug("Maintainer Address", maintainerAddr);
   console.debug("Githoney Address", await lucid.wallet.address());
   // New tx to pay to the contract the minAda and mint the admin, githoney, developer and mantainer tokens
-  console.debug("amount", amount);
+  console.debug("rewards", rewards);
   lucid.selectWalletFrom({ address: maintainerAddr });
+
+  const bountyInfo = {
+    maintainer_addr: maintainerAddr,
+    admin_addr: githoneyAdrr,
+    bounty_id: bounty_id,
+    deadline: deadline,
+    merged: false,
+    fee_reward: fee
+  };
+
+  const bountyDatum = Data.to<BountyDatumT>(
+    bountyInfo,
+    BountyDatum as unknown as BountyDatumT
+  );
+
   const tx = await lucid
     .newTx()
     .collectFrom([utxo])
-    .payToContract(validatorAddress, Data.void(), { lovelace: amount })
-    .payToAddress(maintainerAddr, mantainerAssets)
+    .payToContract(
+      validatorAddress,
+      { inline: bountyDatum },
+      {
+        lovelace: rewards["lovelace"] > MIN_ADA ? rewards["lovelace"] : MIN_ADA,
+        ...rewards
+        // TODO: pay control token to the contract
+      }
+    ) // NOTE this rewrite the lovelace amount?
+    .payToAddress(maintainerAddr, { lovelace: fee, ...mantainerAssets })
+    .payToAddress(githoneyAdrr, { lovelace: fee })
     .addSignerKey(maintainerPkh)
     .addSignerKey(githoneyPkh) // what if maintainer is the same as githoney? Error
     .mintAssets(mintAssets, Data.void())
