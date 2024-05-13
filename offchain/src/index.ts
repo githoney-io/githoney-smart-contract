7d695eb8d713ca5d3f2dce63d25ff64e0da3eac9
Benja272 <bm0130647@gmail.com>
2024-05-13 16:24:41 -0300

    Export new operation

--- offchain/src/index.ts
+++ offchain/src/index.ts
@@ -2,5 +2,12 @@ import createBounty from "./operations/create";
 import closeBounty from "./operations/close";
 import depositBounty from "./operations/deposit";
 import mergeBounty from "./operations/merge";
+import assignContributor from "./operations/assignContributor";
 
-export { createBounty, closeBounty, depositBounty, mergeBounty };
+export {
+  createBounty,
+  closeBounty,
+  depositBounty,
+  mergeBounty,
+  assignContributor
+};
