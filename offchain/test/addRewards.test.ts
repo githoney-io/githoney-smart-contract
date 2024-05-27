88a961f135ff7e10059396575bc20727b51dba7e
sofia-bobbiesi <sofiabobbiesi@gmail.com>
2024-05-26 23:08:04 -0300

    Refactor addRewards function and update test

--- offchain/test/addRewards.test.ts
+++ offchain/test/addRewards.test.ts
@@ -14,31 +14,31 @@ import { signAndSubmitCreate, signAndSubmitAddRewards } from "./utils";
 
 const lucid = await Lucid.new(emulator, "Custom");
 
-const now = new Date();
-const deadline = new Date(now.getTime() + 1000 * 60 * 60 * 24 * 1).getTime(); // Tomorrow
-const createTx = await createBounty(
-  ACCOUNT_MANTAINER.address,
-  ACCOUNT_ADMIN.address,
-  {
-    unit: "lovelace",
-    amount: 100n
-  },
-  BigInt(deadline),
-  bounty_id,
-  lucid
-);
-const { createTxId } = await signAndSubmitCreate(lucid, createTx);
-emulator.awaitBlock(3);
-
 describe("Add Rewards tests", async () => {
-  it("Add Rewards", async () => {
+  const now = new Date();
+  const deadline = new Date(now.getTime() + 1000 * 60 * 60 * 24 * 1).getTime(); // Tomorrow
+  console.log("Deadline", deadline);
+  const createTx = await createBounty(
+    ACCOUNT_MANTAINER.address,
+    ACCOUNT_ADMIN.address,
+    {
+      unit: "lovelace",
+      amount: 100n
+    },
+    BigInt(deadline),
+    bounty_id,
+    lucid
+  );
+  const { createTxId } = await signAndSubmitCreate(lucid, createTx);
+  emulator.awaitBlock(3);
+
+  it("Add Rewards with same token", async () => {
     const [bountyUTxO] = await lucid.utxosByOutRef([
       { txHash: createTxId, outputIndex: 0 }
     ]);
-    console.log("Bounty UTXO", bountyUTxO);
     const reward = {
-      unit: tokenAUnit,
-      amount: 100n
+      unit: "lovelace",
+      amount: 50n
     };
     const addRewardsTx = await addRewards(
       bountyUTxO,
@@ -48,4 +48,39 @@ describe("Add Rewards tests", async () => {
     );
     signAndSubmitAddRewards(lucid, addRewardsTx);
   });
+
+  it("Add Rewards with different token", async () => {
+    const [bountyUTxO] = await lucid.utxosByOutRef([
+      { txHash: createTxId, outputIndex: 0 }
+    ]);
+    const reward = {
+      unit: tokenAUnit,
+      amount: 75n
+    };
+    const addRewardsTx = await addRewards(
+      bountyUTxO,
+      ACCOUNT_0.address,
+      reward,
+      lucid
+    );
+    signAndSubmitAddRewards(lucid, addRewardsTx);
+  });
+
+  // it("Add Rewards with already merged bounty", async () => {
+  // TODO - Add merged bounty
+  //   const [bountyUTxO] = await lucid.utxosByOutRef([
+  //     { txHash: txId.createTxId, outputIndex: 0 }
+  //   ]);
+  //   const reward = {
+  //     unit: tokenAUnit,
+  //     amount: 100n
+  //   };
+  //   const addRewardsTx = await addRewards(
+  //     bountyUTxO,
+  //     ACCOUNT_0.address,
+  //     reward,
+  //     lucid
+  //   );
+  //   signAndSubmitAddRewards(lucid, addRewardsTx);
+  // });
 });
