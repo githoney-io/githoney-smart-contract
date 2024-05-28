23ce060364e24166f257ba9b5ed8c621a806d0a4
sofia-bobbiesi <sofiabobbiesi@gmail.com>
2024-05-28 13:47:05 -0300

    Refactor assignContributor

--- offchain/test/assign.test.ts
+++ offchain/test/assign.test.ts
@@ -1,41 +1,19 @@
 import { describe, it } from "mocha";
 import {
-  ACCOUNT_ADMIN,
-  ACCOUNT_MANTAINER,
-  bounty_id,
   emulator,
   signAndSubmit,
-  ACCOUNT_CONTRIBUTOR
+  ACCOUNT_CONTRIBUTOR,
+  newBounty
 } from "./emulatorConfig";
 import { Lucid, OutRef } from "lucid-cardano";
 import { assignContributor } from "../src/operations/assignContributor";
-import { createBounty } from "../src/operations/create";
 
 const lucid = await Lucid.new(emulator, "Custom");
 
 describe("Assign Contributor tests", async () => {
-  const now = new Date();
-  const deadline = new Date(now.getTime() + 1000 * 60 * 60 * 24 * 1).getTime(); // Tomorrow
-  console.log("Deadline", deadline);
-  const createTx = await createBounty(
-    ACCOUNT_MANTAINER.address,
-    ACCOUNT_ADMIN.address,
-    {
-      unit: "lovelace",
-      amount: 100n
-    },
-    BigInt(deadline),
-    bounty_id,
-    lucid
-  );
-  emulator.awaitBlock(1);
-
-  lucid.selectWalletFromSeed(ACCOUNT_MANTAINER.seedPhrase);
-  const { txId } = await signAndSubmit(lucid, createTx);
-  emulator.awaitBlock(3);
-
   it("Assign Contributor", async () => {
-    const bountyOutRef: OutRef = { txHash: txId, outputIndex: 0 };
+    const createTx = await newBounty(lucid);
+    const bountyOutRef: OutRef = { txHash: createTx.txId, outputIndex: 0 };
 
     const assignTx = await assignContributor(
       bountyOutRef,
