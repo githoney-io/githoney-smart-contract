beaa0a3bdf528409a4b4ad84b36e4f84b69637c7
sofia-bobbiesi <sofiabobbiesi@gmail.com>
2024-06-26 15:31:40 -0300

    chore: Add test for updating settings

--- offchain/test/settings/update.test.ts
+++ offchain/test/settings/update.test.ts
@@ -7,11 +7,33 @@ import { update } from "../../src/operations/settings/update";
 
 describe("Update Settings Test", async () => {
   it("Update settings", async () => {
-    const settingsUtxo = await deployUtxo(lucid);
+    const { settingsUtxo } = await deployUtxo(lucid);
 
     const tx = await update(settingsUtxo, lucid);
     emulator.awaitBlock(3);
     lucid.selectWalletFromSeed(ACCOUNT_GITHONEY.seedPhrase);
     await signAndSubmit(lucid, tx);
+  });
+
+  it("Update with wrong settings", async () => {
+    const { settingsUtxo } = await deployUtxo(lucid);
+    try {
+      const settings = {
+        githoneyWallet: {
+          paymentKey: "paymentKey",
+          stakeKey: "stakeKey"
+        },
+        creationFee: 1000000n,
+        rewardFee: 1000n
+      };
+      const tx = await update(settingsUtxo, lucid, settings);
+      emulator.awaitBlock(3);
+      lucid.selectWalletFromSeed(ACCOUNT_GITHONEY.seedPhrase);
+      await signAndSubmit(lucid, tx);
+    } catch (e) {
+      const error = e as Error;
+      logger.error(error.message);
+      expect(error.message).to.contain("Error: Could not serialize the data");
+    }
   });
 });
