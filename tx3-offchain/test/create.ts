d4bb2958bcea8e0cd86541d88f07c1c7df4ba826
sofia-bobbiesi <sofiabobbiesi@gmail.com>
2025-07-04 14:37:49 -0300

    feat: update initial_value structure in GithoneyDatum and adjust create transaction logic

--- tx3-offchain/test/create.ts
+++ tx3-offchain/test/create.ts
@@ -4,11 +4,14 @@ import { lucidBase, lucidWithWallet } from "../utils/utils";
 
 const rewardPolicy = "fb279c09175731ade05f7314a9b36cf923c7a3d6873be26bbd1eeccf";
 const rewardName = "tokenD";
-const rewardAmount = 1000000n;
+const rewardAmount = 1_000n;
 const bountyId = "bountyTX3";
 
 const [settingsUtxo] = await lucidBase.utxosByOutRef([
-  { txHash: "", outputIndex: 0 },
+  {
+    txHash: "d05c0710320cb81acebd199fea94471a6c0b559617b6ca061dace021fbb59b3d",
+    outputIndex: 0,
+  },
 ]);
 
 const { createCbor } = await createBounty(
