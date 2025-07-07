e777bd202efb71d68ee73a6506cbcd4703892638
sofia-bobbiesi <sofiabobbiesi@gmail.com>
2025-07-07 13:35:34 -0300

    implement addReward function

---
+++ tx3-offchain/operations/bounties/addRewards.ts
@@ -0,0 +1,49 @@
+import { protocol } from "../../gen/typescript/protocol.ts";
+import { Utxo } from "@spacebudz/lucid";
+import { GithoneyContractSettingsSpend } from "../../plutus.ts";
+import { lucidBase, lucidWithWallet } from "../../utils/utils.ts";
+import { selectUTxOs, sortUTxOs } from "../../utils/utxo.ts";
+
+async function addReward(
+  userAddr: string,
+  rewardPolicy: string,
+  rewardName: string,
+  rewardAmount: bigint,
+  settingsUtxo: Utxo,
+  bountyUtxo: Utxo,
+): Promise<{
+  addRewardCbor: string;
+}> {
+  const script = new GithoneyContractSettingsSpend();
+  const scriptAddress = lucidBase.utils.scriptToAddress(script);
+
+  const selectedUtxos = await lucidWithWallet.wallet
+    .getUtxos()
+    .then((utxos) => selectUTxOs(utxos, { lovelace: 10_000_000n }))
+    .then((utxos) => sortUTxOs(utxos, "Canonical"));
+
+  const collateralref =
+    selectedUtxos[0].txHash + "#" + selectedUtxos[0].outputIndex;
+
+  const now = new Date().getTime();
+  const sixHoursFromNow = new Date(now + 30 * 60 * 60 * 1000).getTime();
+
+  const { tx } = await protocol.addTx({
+    script: scriptAddress,
+    user: userAddr,
+    settingsref: `${settingsUtxo.txHash}#${settingsUtxo.outputIndex}`,
+    bountyref: `${bountyUtxo.txHash}#${bountyUtxo.outputIndex}`,
+    rewardpolicyid: Buffer.from(rewardPolicy, "hex"),
+    rewardassetname: Buffer.from(rewardName),
+    rewardamount: Number(rewardAmount),
+    since: lucidBase.utils.unixTimeToSlots(now),
+    until: lucidBase.utils.unixTimeToSlots(sixHoursFromNow),
+    collateralref: collateralref,
+  });
+
+  return {
+    addRewardCbor: tx,
+  };
+}
+
+export { addReward };
