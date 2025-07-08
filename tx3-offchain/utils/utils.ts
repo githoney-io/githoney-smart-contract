80a06ab1001ecbae5352b1f2d7675a5be23be8ce
sofia-bobbiesi <sofiabobbiesi@gmail.com>
2025-07-08 14:45:19 -0300

    feat: add deploy script

--- tx3-offchain/utils/utils.ts
+++ tx3-offchain/utils/utils.ts
@@ -14,3 +14,15 @@ export const lucidBase = new Lucid({
 export const lucidWithWallet = lucidBase.selectWalletFromSeed(
   process.env.SEED as string,
 );
+
+export const cExplorerTxURL = "https://preprod.cexplorer.io/tx/";
+
+export const signAndSubmit = async (cbor: string): Promise<string> => {
+  lucidBase.selectWalletFromSeed(process.env.SEED as string);
+  const tx = await lucidWithWallet.fromTx(cbor);
+  const signedTx = await tx.sign().commit();
+  const txHash = await signedTx.submit();
+  console.log("Submitted transaction. View it at: " + cExplorerTxURL + txHash);
+
+  return txHash;
+};
