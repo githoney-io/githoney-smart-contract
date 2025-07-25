ef15fe1f35928ea3aad29fe87659ffa630670445
sofia-bobbiesi <sofiabobbiesi@gmail.com>
2025-07-25 15:35:16 -0300

    feat: Refactor scripts

--- tx3-offchain/test/deploy.ts
+++
@@ -1,8 +0,0 @@
-import { deploySettings } from "../../offchain/src";
-import { githoneyAddr } from "../constants";
-import { lucidWithWallet, signAndSubmit } from "../utils/utils";
-
-const { cbor, outRef } = await deploySettings(githoneyAddr, lucidWithWallet);
-await signAndSubmit(cbor);
-console.log("Settings deployed successfully.");
-console.log("Output reference:", outRef);
