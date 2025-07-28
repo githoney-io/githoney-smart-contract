c6170e99246e8092bae71b54ef63ba7c26815536
sofia-bobbiesi <sofiabobbiesi@gmail.com>
2025-07-28 14:06:36 -0300

    feat: add merge bounty tests

---
+++ tx3-offchain/test/bounties/merge.test.ts
@@ -0,0 +1,74 @@
+import { describe, expect, it } from "@jest/globals";
+import { OutRef } from "@spacebudz/lucid";
+import { logger, newAssign, newBounty, waitForUtxosUpdate } from "../utils";
+import { mergeBounty } from "../../operations";
+import { lucidBase, signAndSubmit } from "../../utils/utils";
+import { adminAddr, adminSeed, settingsRef } from "../../constants";
+
+describe("Merge tests", async () => {
+  it("Merge bounty", async () => {
+    const [settingsUtxo] = await lucidBase.utxosByOutRef([settingsRef]);
+    const createTxHash = await newBounty(lucidBase, settingsUtxo);
+    const createOutRef: OutRef = { txHash: createTxHash, outputIndex: 0 };
+
+    const assignTxHash = await newAssign(lucidBase, createOutRef, settingsUtxo);
+    const assignOutRef: OutRef = { txHash: assignTxHash, outputIndex: 0 };
+
+    const { mergeCbor } = await mergeBounty(
+      adminAddr,
+      settingsUtxo,
+      assignOutRef,
+    );
+    lucidBase.selectWalletFromSeed(adminSeed);
+    const mergeTxHash = await signAndSubmit(mergeCbor, lucidBase);
+    waitForUtxosUpdate(lucidBase, mergeTxHash);
+  });
+
+  it("Merge bounty already merged", async () => {
+    const [settingsUtxo] = await lucidBase.utxosByOutRef([settingsRef]);
+    try {
+      const createTxHash = await newBounty(lucidBase, settingsUtxo);
+      const createOutRef: OutRef = { txHash: createTxHash, outputIndex: 0 };
+
+      const assignTxHash = await newAssign(
+        lucidBase,
+        createOutRef,
+        settingsUtxo,
+      );
+      const assignOutRef: OutRef = { txHash: assignTxHash, outputIndex: 0 };
+
+      // First merge
+      const { mergeCbor } = await mergeBounty(
+        adminAddr,
+        settingsUtxo,
+        assignOutRef,
+      );
+      lucidBase.selectWalletFromSeed(adminSeed);
+      const mergeTxHash = await signAndSubmit(mergeCbor, lucidBase);
+      waitForUtxosUpdate(lucidBase, mergeTxHash);
+
+      // Second merge
+      const mergeOutRef: OutRef = { txHash: mergeTxHash, outputIndex: 0 };
+      await mergeBounty(adminAddr, settingsUtxo, mergeOutRef);
+    } catch (e) {
+      const error = e as Error;
+      logger.error(error.message);
+      expect(error.message).toBe("Bounty already merged");
+    }
+  });
+
+  it("Merge bounty without contributor", async () => {
+    const [settingsUtxo] = await lucidBase.utxosByOutRef([settingsRef]);
+    try {
+      const createTxHash = await newBounty(lucidBase, settingsUtxo);
+      const createOutRef: OutRef = { txHash: createTxHash, outputIndex: 0 };
+
+      logger.info(`Bounty created with txId: ${createTxHash}`);
+      await mergeBounty(adminAddr, settingsUtxo, createOutRef);
+    } catch (e) {
+      const error = e as Error;
+      logger.error(error.message);
+      expect(error.message).toBe("Bounty doesn't have a contributor");
+    }
+  });
+});
