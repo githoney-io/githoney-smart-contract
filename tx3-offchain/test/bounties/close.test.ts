079218244ed6e272976e2aa8a3c363ecccef6a86
sofia-bobbiesi <sofiabobbiesi@gmail.com>
2025-07-31 15:03:16 -0300

    refactor: update close bounty tests

--- tx3-offchain/test/bounties/close.test.ts
+++ tx3-offchain/test/bounties/close.test.ts
@@ -1,14 +1,13 @@
 import { describe, expect, it } from "@jest/globals";
-import { OutRef } from "@spacebudz/lucid";
+import { Assets, OutRef } from "@spacebudz/lucid";
 import {
-  logger,
   newAssign,
   newBounty,
   newMerge,
-  waitForUtxosUpdate,
+  signSubmitAndWaitConfirmation,
 } from "../utils.ts";
 import { closeBounty, addRewards } from "../../operations/index.ts";
-import { lucidBase as lucid, signAndSubmit } from "../../utils/utils.ts";
+import { logger, lucidBase as lucid } from "../../utils/utils.ts";
 import {
   adminAddr,
   adminSeed,
@@ -33,9 +32,8 @@ describe("Close tests", () => {
       assignOutRef,
     );
     lucid.selectWalletFromSeed(adminSeed);
-    const txId = await signAndSubmit(closeCbor, lucid);
-    waitForUtxosUpdate(lucid, txId);
-  });
+    await signSubmitAndWaitConfirmation(closeCbor, lucid);
+  }, 300000);
 
   it("Close Bounty Before Contributor Assignment", async () => {
     const [settingsUtxo] = await lucid.utxosByOutRef([settingsRef]);
@@ -49,9 +47,8 @@ describe("Close tests", () => {
       createOutRef,
     );
     lucid.selectWalletFromSeed(adminSeed);
-    const txId = await signAndSubmit(closeCbor, lucid);
-    waitForUtxosUpdate(lucid, txId);
-  });
+    await signSubmitAndWaitConfirmation(closeCbor, lucid);
+  }, 300000);
 
   it("Close Bounty already merged", async () => {
     const [settingsUtxo] = await lucid.utxosByOutRef([settingsRef]);
@@ -71,7 +68,7 @@ describe("Close tests", () => {
       logger.error(error.message);
       expect(error.message).toBe("Bounty already merged");
     }
-  });
+  }, 300000);
 
   it("Close Bounty after adding rewards", async () => {
     const [settingsUtxo] = await lucid.utxosByOutRef([settingsRef]);
@@ -81,28 +78,59 @@ describe("Close tests", () => {
     const assignTxId = await newAssign(lucid, createOutRef, settingsUtxo);
     const assignOutRef: OutRef = { txHash: assignTxId, outputIndex: 0 };
 
+    const rewardAmount = 100n;
     const { addRewardCbor } = await addRewards(
-      100n,
+      rewardAmount,
       settingsUtxo,
       sponsorAddr,
       assignOutRef,
     );
     lucid.selectWalletFromSeed(sponsorSeed);
-    const addRewardsTxId = await signAndSubmit(addRewardCbor, lucid);
-    waitForUtxosUpdate(lucid, addRewardsTxId);
+    const addRewardsTxId = await signSubmitAndWaitConfirmation(
+      addRewardCbor,
+      lucid,
+    );
     const addRewardsOutRef: OutRef = {
       txHash: addRewardsTxId,
       outputIndex: 0,
     };
 
+    const rewardUnit =
+      "fb279c09175731ade05f7314a9b36cf923c7a3d6873be26bbd1eeccf746f6b656e44";
+
+    const refundings: { [key: string]: Assets } = {
+      [sponsorAddr]: {
+        [rewardUnit]: rewardAmount,
+      },
+    };
+
     const { closeCbor } = await closeBounty(
       adminAddr,
-      {}, // REVIEW
+      refundings,
       settingsUtxo,
       addRewardsOutRef,
     );
     lucid.selectWalletFromSeed(adminSeed);
-    const txId = await signAndSubmit(closeCbor, lucid);
-    waitForUtxosUpdate(lucid, txId);
-  });
+    await signSubmitAndWaitConfirmation(closeCbor, lucid);
+  }, 300000);
+
+  it("Close Bounty already merged", async () => {
+    const [settingsUtxo] = await lucid.utxosByOutRef([settingsRef]);
+    try {
+      const createTxId = await newBounty(lucid, settingsUtxo);
+      const createOutRef: OutRef = { txHash: createTxId, outputIndex: 0 };
+
+      const assignTxId = await newAssign(lucid, createOutRef, settingsUtxo);
+      const assignOutRef: OutRef = { txHash: assignTxId, outputIndex: 0 };
+
+      const mergeTxId = await newMerge(lucid, assignOutRef, settingsUtxo);
+      const mergeOutRef: OutRef = { txHash: mergeTxId, outputIndex: 0 };
+
+      await closeBounty(adminAddr, {}, settingsUtxo, mergeOutRef);
+    } catch (e) {
+      const error = e as Error;
+      logger.error(error.message);
+      expect(error.message).toBe("Bounty already merged");
+    }
+  }, 300000);
 });
